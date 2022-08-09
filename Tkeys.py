8e213f3b200e4f88f83715aff49e75ec642c6717
Ude Fortune <chiziude8@gmail.com>
2022-08-09 12:56:20 +0100

    Add files via upload

---
+++ Tkeys.py
@@ -0,0 +1,8 @@
+bearer_token = 'AAAAAAAAAAAAAAAAAAAAAAEbeQEAAAAAzl3m4cYOycwBRZk44u9z9RjSsug%3DeHSZUlsY9HJUoMJNGCyicFLnB0QIll53w7YG84wHXNpueshcj1'
+consumer_key = '0AEhlVu8swJarzkbJK7srnpIm'
+consumer_secret= '8k7T8UDY8WjV6A4SpbfGuZBdzExWes3wNMZJcWwwVCNbLgVBeA'
+access_token= '815569178069499905-nwBFXEdR9tds3KOm8JqPHK9q9Z4mOeR'
+access_secret= '0rNB9e2lnqiZhLKqXdUtmGz94dfwiv8rGlzTPCwIaDtL4'
+
+
+
